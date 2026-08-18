460ee0af67e81fe646be4beeda1bf3d8986a5370
tvtteam2025-dev <tvtteam2025@gmail.com>
2026-08-18 10:23:04 +0700

    Update api.js

--- js/api.js
+++ js/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://script.google.com/macros/s/AKfycbwY7VhVKTnv03nLoqul2PLwB8mhto8aFlVuJTI5Hvqe8nBaqyRcKpFBJqFmKFr-SSuY/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbx48zjZAhg4rxyooBlKvY0-1L5FVnWn37QgGyP_mRCnjMP3XChRKsOdPvqzOiOcND1O/exec';
 
 const API = {
     fetchData: async function (id, hopDongId, template, username, password) {
